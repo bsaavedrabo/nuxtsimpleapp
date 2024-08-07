1e0895bdbd8a6eb0ba03941985d7437378330bc1
Belen Saavedra <saavedrariosm@berea.edu>
2024-08-07 11:43:19 -0400

    chatgpt module

---
+++ components/training.vue
@@ -0,0 +1,81 @@
+<template>
+    <div>
+      <br/>
+      <p>Training latency is the time it takes to train a machine learning model on a dataset. For a flower classification model on a handheld device, low training latency is crucial for quickly updating the model with new data, ensuring it remains accurate and effective without causing extended delays in development or updates.</p>
+      <br/>
+      <!-- 1st stage-->
+      <h2><b> </b></h2>
+
+      <label><b>Deployment Infrastructure</b></label>
+      <USelect
+        placeholder="Select an option..."
+        :options="['Cloud', 'On-premise', 'Edge']"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        v-model="infrastructureDetails"
+      />
+      <p>Info Tip:  </p>
+      <br/>
+   
+
+      <label><b>Frequency of retraining </b></label>
+      <USelect
+        placeholder="Select an option..."
+        :options="['Never', 'TBD']"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        v-model="strategies"
+      />
+      <p>Info Tip:  </p>
+      <br/>
+
+      <label><b> Are you considering energy usage and carbon footprint in your model? 
+    </b></label>
+      <USelect
+        placeholder="Select an option..."
+        :options="['Yes', 'No']"
+        icon="i-heroicons-magnifying-glass-20-solid"
+        v-model="strategies"
+      />
+      <p>Info Tip:  </p>
+      <br/>
+
+      <label><b> Trade-offs to Consider:</b></label>
+      <ul>
+        <li><b> - Model Accuracy vs. Training Speed:</b></li>
+        <li><b> - Feature Engineering vs. Training Speed</b> </li>
+    </ul>
+
+    <br/>
+
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { ref } from 'vue';
+  
+  export default {
+    name: 'InferenceLatencyForm',
+    props: {
+      MLTask: {
+        type: String,
+        required: true,
+      },
+      usageContext: {
+        type: String,
+        required: true,
+      },
+    },
+    setup() {
+      const deploymentInfrastructure = ref<string | null>(null);
+      const infrastructureDetails = ref<string | null>(null);
+  
+      return {
+        deploymentInfrastructure,
+        infrastructureDetails,
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-chatgpt"],
+
+  modules: ["nuxt-chatgpt", "@nuxt/ui"],
   chatgpt: {
     apiKey: process.env.OPENAI_API_KEY
   },
 })
-
